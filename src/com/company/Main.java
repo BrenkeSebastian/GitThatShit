467175050b0832388fb376cc0c74cd9d07b31014
sebastian <sebastian.brenke@gmx.de>
2020-02-26 14:37:49 +0100

    Implamentation of the Insult generator

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import java.sql.Time;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        long x;
+        for(int i=0; i<5; i++) {
+            x = System.currentTimeMillis();
+            if (x % 3 == 0) {
+                System.out.println("Nick ist dumm");
+            }
+            if (x % 2 == 0) {
+                System.out.println("Git ist dumm");
+            }
+            if (x % 7 == 0) {
+                System.out.println("Sebastian ist dumm");
+            }
+        }
+
+
     }
 }
